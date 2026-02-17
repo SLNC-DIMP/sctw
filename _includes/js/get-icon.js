1dc3546afe507e382e74b71e7ff03ac321e627b3
asmythe81 <digital.info@dncr.nc.gov>
2026-02-17 16:35:40 -0500

    Update metadata and add html icon

--- _includes/js/get-icon.js
+++ _includes/js/get-icon.js
@@ -27,6 +27,9 @@ function getIcon(objectTemplate,objectFormat,svgType) {
     } else if (iconTemplate.includes("audio")) {
         iconId = "icon-audio";
         iconTitle = "audio file icon";
+    } else if (iconTemplate.includes("html")) { 
+        iconId = "icon-html";
+        iconTitle = "html file icon";
     } else if (iconTemplate.includes("panorama")) {
         iconId = "icon-panorama";
         iconTitle = "panorama file icon";
